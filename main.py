3039b7a50cd4ed3629468959269dd0fe9f1dfce9
qwerty <cool.arsic@yandex.com>
2022-04-28 13:19:31 +0200

    Загрузка файла

---
+++ main.py
@@ -0,0 +1,79 @@
+import random
+
+from flask import Flask, url_for, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def root():
+    return "Миссия Колонизация Марса"
+
+
+@app.route('/index')
+def index():
+    return "И на Марсе будут яблони цвести!"
+
+
+@app.route('/promotion')
+def promotion():
+    text = """Человечество вырастает из детства.</br>
+
+Человечеству мала одна планета.</br>
+
+Мы сделаем обитаемыми безжизненные пока планеты.</br>
+
+И начнем с Марса!</br>
+
+Присоединяйся!</br>"""
+    return text
+
+
+@app.route('/promotion_image')
+def promotion_image():
+    img = url_for('static', filename='img/mars.jpg')
+    styles = url_for('static', filename='css/style.css')
+    return render_template('index.html', img=img, style=styles)
+
+
+@app.route('/form_sample', methods=['POST', 'GET'])
+def form_sample():
+    if request.method == 'GET':
+        styles = url_for('static', filename='css/style.css')
+        return render_template('form.html', style=styles)
+    elif request.method == 'POST':
+        return "Форма отправлена"
+
+
+@app.route('/choice/<planet_name>')
+def choice(planet_name):
+    styles = url_for('static', filename='css/style.css')
+    if planet_name.lower() == 'земля':
+        return render_template('zemlya.html', planet=planet_name, style=styles)
+    elif planet_name.lower() == 'марс':
+        return render_template('mars.html', planet=planet_name, style=styles)
+    else:
+        return render_template('another_planet.html', planet=planet_name, style=styles)
+
+
+@app.route('/results/<nickname>/<int:level>/<float:rating>')
+def results(nickname, level, rating):
+    styles = url_for('static', filename='css/style.css')
+    return render_template('results.html', nickname=nickname, level=level, rating=rating, style=styles)
+
+
+@app.route('/load_photo', methods=['POST', 'GET'])
+def load_photo():
+    styles = url_for('static', filename='css/style.css')
+    if request.method == 'GET':
+        img = url_for('static', filename='img/loaded_photo.png')
+        return render_template('image_form.html', img=img, style=styles)
+    elif request.method == 'POST':
+        f = request.files['file']
+        with open('static//img//loaded_photo.png', 'wb') as photo:
+            photo.write(f.read())
+        return "Форма отправлена"
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
